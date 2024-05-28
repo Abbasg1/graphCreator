dd2be548a38dee90f2b38c4db7da2d1dff41aaa3
“412140” <412140@bsd48.org>
2024-05-28 01:44:26 +0100

    remove edges and vertex

--- main.cpp
+++ main.cpp
@@ -26,7 +26,40 @@ void addAdj(int t[][20], vector<char*> v, char* first, char* second, int w)
     }
     count++;
   }
-  
+  t[row][column]= w;
+}
+
+vector<char*> removeAdjVertex (vector<char*> vs, char* v)
+{//vertices and vertex to be removed from adj table
+  vector <char*>::iterator it;
+  for(it = vs.begin(); it < vs.end(); it++)
+  {
+    if(strcmp(*it, v)==0)
+    {
+      vs.erase(it);
+      cout << "removed vertex"<< endl;
+      break;
+    }
+  }
+  return v;
+}
+
+void removeAdjEdge(int t[][20], vector<char*> vs, char* first, char* second)
+{
+  int count, row, column = 0;
+  for(vector<char*>::iterator it = vs.begin(); it < vs.end(); it++)
+  {
+    if(strcmp(*it, first)==0)
+    {
+      column = count;
+    }
+    else if(strcmp(*it, second)==0)
+    {
+      row = count;
+    }
+    count++;
+  }
+  t[r][c] = -1;
 }
 
 
